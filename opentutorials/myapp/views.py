aeb45a7cf7bc352061b7f541f57fd42380f11c50
Dan Kim <lololtoday@gmail.com>
2022-02-01 00:48:32 +0900

    chore: add content

--- opentutorials/myapp/views.py
+++ opentutorials/myapp/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render, HttpResponse
 
+topics = [
+    {"id": 1, "title": "routing", "body": "routing is.."},
+    {"id": 2, "title": "view", "body": "view is.."},
+    {"id": 3, "title": "model", "body": "model is.."},
+]
+
 
 def index(request):
-    return HttpResponse("welcome to index page.")
+    global topics
+    ol_content = "".join(
+        [
+            f"<li><a href='/read/{topic['id']}'>{topic['title']}</a></li>"
+            for topic in topics
+        ]
+    )
+
+    return HttpResponse(
+        f"""
+    <html>
+        <body>
+            <h1>Django</h1>
+            <ol>
+            {ol_content}
+            </ol>
+            <h2>Welcome</h2>
+            <p>hello, django</p>
+        </body>
+    </html>
+    """
+    )
 
 
 def create(request):
