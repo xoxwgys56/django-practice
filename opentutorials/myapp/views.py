e1fee0ebf3f7228ce4232d416bad161bbaae5b8f
Daewon Kim <lololtoday@gmail.com>
2022-02-21 15:58:35 +0900

    chore: add create job

--- opentutorials/myapp/views.py
+++ opentutorials/myapp/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from django.shortcuts import redirect, render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+
+if TYPE_CHECKING:
+    from django.http.request import HttpRequest
 
 topics = [
     {"id": 1, "title": "routing", "body": "routing is.."},
     {"id": 2, "title": "view", "body": "view is.."},
     {"id": 3, "title": "model", "body": "model is.."},
 ]
+
+next_id = len(topics) + 1
 
 
 def get_html_template(article_tag):
@@ -39,19 +46,33 @@ def index(request):
 
 
 @csrf_exempt
-def create(request):
-    article = f"""
+def create(request: HttpRequest):
+    global next_id
+
+    if request.method == "GET":
+        article = f"""
         <form action="/create/" method="post">
             <p><input type="text" name="title" placeholder="title" /></p>
             <p><textarea name="body" placeholder="body" ></textarea></p>
             <p><input type="submit" /></p>
         </form>
-    """
+        """
+        return HttpResponse(get_html_template(article))
+    elif request.method == "POST":
+        topics.append(
+            {
+                "id": next_id,
+                "title": request.POST["title"],
+                "body": request.POST["body"],
+            }
+        )
+        my_id = next_id
+        next_id = next_id + 1
 
-    return HttpResponse(get_html_template(article))
+        return redirect(f"/read/{my_id}")
 
 
-def read(request, id: str):
+def read(request: HttpRequest, id: str):
     global topics
     article = f"""
         <h2>Not Found</h2>
