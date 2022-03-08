bf08ac7af06502f2e1815b5cca5978debb7084dd
Daewon Kim <lololtoday@gmail.com>
2022-03-08 17:13:12 +0900

    chore: update progress

---
+++ askcompany/drf/askcompany/instagram/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register("post", views.PostViewSet)
+
+
+urlpatterns = [
+    path("public/", views.PublicPostListAPIView.as_view()),
+    path("", include(router.urls)),
+]
--- askcompany/drf/askcompany/instagram/views.py
+++ askcompany/drf/askcompany/instagram/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PublicPostListAPIView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
